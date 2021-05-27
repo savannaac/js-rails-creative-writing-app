82d3d5fc0f6b07e0cfb2d1f183406621e1629051
savanna chiang <savannaac@gmail.com>
2021-05-27 01:14:33 -0500

    sets up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :topics
+  namespace :api do
+    namespace :v1 do
+      resources :topics do
+        resources :posts
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
