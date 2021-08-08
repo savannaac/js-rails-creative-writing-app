448bb98fe37846dac75e4e71ecf6f234e70199f5
savanna chiang <savannaac@gmail.com>
2021-08-07 23:59:40 -0500

    cleans things up

--- src/components/topics.js
+++ src/components/topics.js
@@ -1,25 +1,24 @@
 class Topics {
 
-    constructor() {
-        this.baseUrl = "http://127.0.0.1:3000/api/v1/posts"
-        this.topics = []
-        this.adapter = new TopicsAdapter()
-        // this.id = id
-        // this.description = description;
-        // this.card = this.createCard()
-        this.bindingsAndEventListeners()
-        this.fetchAndLoadTopics()
+    constructor(id, description) {
+        this.baseUrl = "http://127.0.0.1:3000/api/v1/posts";
+        this.topics = [];
+        this.adapter = new TopicsAdapter();
+        this.id = id;
+        this.description = description;
+        this.bindingsAndEventListeners();
+        this.fetchAndLoadTopics();
     }
 
     bindingsAndEventListeners() {
-        this.topicForm = document.getElementById("topic-form")
-        this.topicInput = document.getElementById("topic-input")
-        this.topicNode = document.getElementById("topic-form-container")
-        this.topicsContainer = document.getElementById("topics")
+        this.topicForm = document.getElementById("topic-form");
+        this.topicInput = document.getElementById("topic-input");
+        this.topicNode = document.getElementById("topic-form-container");
+        this.topicsContainer = document.getElementById("topics");
 
-        this.postContainer = document.getElementById("posts")
+        this.postContainer = document.getElementById("posts");
 
-        this.topicForm.addEventListener("submit", this.addTopic)
+        this.topicForm.addEventListener("submit", this.addTopic);
         // this.topicNode.addEventListener("click", this.deleteTopic.bind(this))
     }
 
@@ -91,34 +90,6 @@ class Topics {
         this.topicsContainer.appendChild(topicDiv)
     
     }
-
-    // getPosts() {
-    //     return fetch(this.baseUrl, {
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Accept": "application/json"
-    //         }
-    //     })
-    //     .then(res => res.json())
-    //     .then(data => data.forEach(post => {
-    //         console.log(post)
-    //         this.displayPost(post)
-    //     }))
-    // }
-
-    // displayPost = (post) => {
-    //     const postDiv = document.createElement("div")
-    //     const postContent = document.createElement("h2")
-    //     postContent.innerText = post.content 
-    //     // postContent.setAttribute("contenteditable", "true")
-    //     // const postDeleteButton = document.createElement("button")
-    //     // postDeleteButton.innerText = "delete"
-    //     // postDeleteButton.addEventListener("click", () {
-    //     //     this.adapter.
-    //     // })
-    //     postDiv.appendChild(postContent)
-    //     this.postContainer.appendChild(postDiv)
-    // }
 }
 //
 // checkforPosts(){
